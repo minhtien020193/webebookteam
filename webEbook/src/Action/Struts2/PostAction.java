0aa15169583ad980e2a6fd4dd6a246005c7bf114
Tien Truong <tientruong@antking.com.vn>
2016-08-18 23:53:16 +0700

    listPost

--- webEbook/src/Action/Struts2/PostAction.java
+++ webEbook/src/Action/Struts2/PostAction.java
@@ -1,4 +1,5 @@
 package Action.Struts2;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import DAO.ChapterDAO;
@@ -17,10 +18,24 @@ public class PostAction {
 	List<ChapterDTO> listChapters;
 	boolean priceEmpty = true;
 	List<String> userComment;
+	boolean noData = false;
+	String postName;
+	File fileUpload;
+	File image;
+	String description;
+	String author;
+	int countChapter;
+	String price;
+	String content;
+	
 
 	public String execute() throws Exception {
 		PostDAO post = new PostDAO();
 		listPost = post.getListPost();
+		return "success";
+	}
+	
+	public String sendCreatePost(){
 		return "success";
 	}
 
@@ -49,6 +64,25 @@ public class PostAction {
 		ChapterDAO chapter = new ChapterDAO();
 		listChapters = chapter.getListChapterByPostId(postId);
 
+		return "success";
+	}
+	
+	public String createPost(){
+		postDTO.setAuthorName(author);
+		postDTO.setContents(content);
+		postDTO.setCountChapter(countChapter);
+		
+		int userId = 1;
+		return "success";
+	}
+	
+	public String listPost(){
+		int userId = 1;
+		PostDAO post = new PostDAO();
+		listPost = post.getListPostbyUserId(userId);
+		if(listPost.isEmpty()){
+			noData = true;
+		}
 		return "success";
 	}
 
@@ -107,6 +141,78 @@ public class PostAction {
 
 	public void setUserComment(List<String> userComment) {
 		this.userComment = userComment;
+	}
+
+	public boolean isNoData() {
+		return noData;
+	}
+
+	public void setNoData(boolean noData) {
+		this.noData = noData;
+	}
+
+	public String getPostName() {
+		return postName;
+	}
+
+	public void setPostName(String postName) {
+		this.postName = postName;
+	}
+
+	public File getFileUpload() {
+		return fileUpload;
+	}
+
+	public void setFileUpload(File fileUpload) {
+		this.fileUpload = fileUpload;
+	}
+
+	public File getImage() {
+		return image;
+	}
+
+	public void setImage(File image) {
+		this.image = image;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getCountChapter() {
+		return countChapter;
+	}
+
+	public void setCountChapter(int countChapter) {
+		this.countChapter = countChapter;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
 	}
 	
 
